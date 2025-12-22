70799186b7655200c31d14cb566f3d6db1950b83
pawndev <coquelet.c@gmail.com>
2025-12-22 17:31:19 +0100

    feat(ui/lang): Add french language to available language choice

--- ui/settings.go
+++ ui/settings.go
@@ -190,6 +190,7 @@ func (s *SettingsScreen) buildMenuItems(config *utils.Config) []gaba.ItemWithOpt
 			Options: []gaba.Option{
 				{DisplayName: i18n.GetString("settings_language_english"), Value: "en"},
 				{DisplayName: i18n.GetString("settings_language_spanish"), Value: "es"},
+				{DisplayName: i18n.GetString("settings_language_french"), Value: "fr"},
 			},
 			SelectedOption: languageToIndex(config.Language),
 		},
